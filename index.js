93eb820eb51b6ef29e8d276a6e44a3c0080d7992
Max Gotts <spamgotts@gmail.com>
2019-05-30 14:14:54 -0700

    -

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const Monero = require('monerojs');
+const sha1 = require('js-sha1');
 
 var daemonRPC = new Monero.daemonRPC({ autoconnect: true })
 .then((daemon) => {
@@ -7,9 +8,19 @@ var daemonRPC = new Monero.daemonRPC({ autoconnect: true })
 	var walletRPC = new Monero.walletRPC() // Connect with defaults
 	.then(wallet => {
 		walletRPC = wallet;
-		walletRPC.create_wallet('monero_test_wallet', '')
+		
+	});
+})
+.catch(err => {
+	throw new Error(err);
+});
+
+
+function createAndOpenWallet() {
+	const walletName = sha1(new Date().getTime())
+	return walletRPC.create_wallet(walletName, '')
 		.then(new_wallet => {
-			walletRPC.open_wallet('monero_test_wallet', '')
+			walletRPC.open_wallet(walletName, '')
 			.then(wallet => {
 				walletRPC.getaddress()
 				.then(balance => {
@@ -26,33 +37,4 @@ var daemonRPC = new Monero.daemonRPC({ autoconnect: true })
 		.catch(err => {
 			console.error(err);
 		});
-	});
-})
-.catch(err => {
-	throw new Error(err);
-});
-
-
-/*
-
-Functions to add:
-
--> createAccount @param <{username<string>, password<string>, credit_card...<{number<string>, expiration<string>}>}> @return response<error>
-==> create Monero address
-==> create instance in database
-==> store user information
-
--> sendTo @param username<string> @return response<error>
-==> (option A) send money to my account
-==> (option A) convert money to Monero
-==> (option A) move Monero to their account
-==> (option B) convert money to Monero
-==> add Monero transaction
-==> reverse (A/B) on the recipient's side
-
--> viewTransactions @param username<string> @return [{recipient<string>, date_time<string>, amount<string>}]
-==> get transactions
-==> convert recipient Monero addresses to usernames
-==> convert date_time to location
-
-*/
+}
