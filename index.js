14c4e29e9fb77cd77c5c152827c9bffaae9de78e
Max Gotts <spamgotts@gmail.com>
2019-06-04 14:22:44 -0700

    -

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const Monero = require('monerojs');
-const sha1 = require('js-sha1');
+//const sha1 = require('js-sha1');
 
 var daemonRPC = new Monero.daemonRPC({ autoconnect: true })
 .then((daemon) => {
@@ -8,7 +8,7 @@ var daemonRPC = new Monero.daemonRPC({ autoconnect: true })
 	var walletRPC = new Monero.walletRPC() // Connect with defaults
 	.then(wallet => {
 		walletRPC = wallet;
-		
+		createAndOpen(walletRPC);	
 	});
 })
 .catch(err => {
@@ -16,15 +16,15 @@ var daemonRPC = new Monero.daemonRPC({ autoconnect: true })
 });
 
 
-function createAndOpenWallet() {
-	const walletName = sha1(new Date().getTime())
+function createAndOpenWallet(walletRPC) {
+	const walletName = "new_wallet_42" //sha1(new Date().getTime())
 	return walletRPC.create_wallet(walletName, '')
 		.then(new_wallet => {
 			walletRPC.open_wallet(walletName, '')
 			.then(wallet => {
 				walletRPC.getaddress()
 				.then(balance => {
-					console.log(balance);
+					console.log("createAndOpenWallet success");
 				})
 				.catch(err => {
 					console.error(err);
