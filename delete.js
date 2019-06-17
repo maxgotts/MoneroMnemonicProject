6130cb8cfba6774391091254f08a828fb3a70d2b
Max Gotts <spamgotts@gmail.com>
2019-06-17 16:17:55 -0700

    readme added and cleanup enacted

--- delete.js
+++
@@ -1,9 +0,0 @@
-var fs = require('fs');
-
-const walletName = "new-wallet"
-
-fs.unlink("./wallets/"+walletName+".keys", function() { // delete previous new-wallet.key
-    fs.unlink("./wallets/"+walletName, function() { // delete previous new-wallet.key
-        console.log("done")
-    });
-});
