f6ade11a70f1f66477ae0d84f2693cf3c580991e
maxgotts <max.gotts@gmail.com>
2019-08-29 13:48:11 -0700

    readme and toolchain

--- README.md
+++ README.md
@@ -1,9 +1,4 @@
-# Mnemonic Generator for Monero
-This creates a mnemonic based on a specific phrase by generating random wallets
-
 Run the following commands in three different terminal windows
 ./monerod 
 ./monero-wallet-rpc --rpc-bind-port 28083 --disable-rpc-login --wallet-dir ../wallets
 node index.js
-
-Download MyMonero and "open an existing wallet by mnemonic" to check that your new mnemonic works.
--- index.js
+++ index.js
@@ -1,56 +1,23 @@
 const Monero = require('monerojs');
-var fs = require('fs');
 
-const TheWalletName = "new-wallet"
-const mnemonicHint = "turtle"
-
-/* Start up the Daemon RPC and Call createMnemonic */
 var daemonRPC = new Monero.daemonRPC({ autoconnect: true })
-	.then((daemon) => {
-		daemonRPC = daemon; // Store daemon interface in global variable
+.then((daemon) => {
+	daemonRPC = daemon; // Store daemon interface in global variable
 
-		var walletRPC = new Monero.walletRPC() // Connect with defaults
-		.then(wallet => {
-			walletRPC = wallet;
-			createMnemonic(walletRPC, mnemonicHint)
-		});
-	})
-	.catch(err => {
-		throw new Error(err);
-	});
-
-/* Create and Open a Wallet */
-function createWallet(walletRPC, walletName, chain) {
-	walletRPC.create_wallet(walletName, '')
-		.then(new_wallet => {
-			console.log("createWallet " + walletName + ".key success");
-			chain(walletRPC, walletName)
-		})
-		.catch(err => {
-			console.error(err);
-		});
-}
-
-function openWallet(walletRPC, walletName, chain) {
-	walletRPC.open_wallet(walletName, '')
+	var walletRPC = new Monero.walletRPC() // Connect with defaults
 	.then(wallet => {
-		console.log("openWallet " + walletName + ".key success");
-		chain(walletRPC)
-	})
-}
-
-function createAndOpenWalletByParts(walletRPC, walletName, chain) {
-	createWallet(walletRPC, walletName, function(walletRPC) {
-		openWallet(walletRPC, walletName, chain)
-	});
-}
-function createAndOpenWalletAllInOne(walletRPC, walletName, chain) {
-	walletRPC.create_wallet(walletName, '')
+		walletRPC = wallet;
+		walletRPC.create_wallet('monero_test_wallet', '')
 		.then(new_wallet => {
-			walletRPC.open_wallet(walletName, '')
+			walletRPC.open_wallet('monero_test_wallet', '')
 			.then(wallet => {
-				console.log("createAndOpenWallet " + walletName + ".key success");
-				chain(walletRPC)
+				walletRPC.getaddress()
+				.then(balance => {
+					console.log(balance);
+				})
+				.catch(err => {
+					console.error(err);
+				});
 			})
 			.catch(err => {
 				console.error(err);
@@ -59,35 +26,8 @@ function createAndOpenWalletAllInOne(walletRPC, walletName, chain) {
 		.catch(err => {
 			console.error(err);
 		});
-}
-function createAndOpenWallet(walletRPC, walletName, chain) { createAndOpenWalletByParts(walletRPC, walletName, chain); } // rename
-
-/* Get the Mnemonic of an Opened Wallet */
-function getMnemonic(walletRPC, keyIncludes, chain) {
-	walletRPC.mnemonic()
-		.then(mnemonic => {
-			console.log("Your mnemonic is: "+mnemonic.key)
-			chain(walletRPC, { keyIncluded: mnemonic.key.includes(keyIncludes), mnemonic:mnemonic.key })
-		})
-		.catch(err => {
-			console.error(err);
-		});
-}
-
-/* Create Wallets again and again until the Mnemonic of that Wallet contains a certain Substring */
-function createMnemonic(walletRPC, keyIncludes) {
-	createAndOpenWallet(walletRPC, TheWalletName, function(walletRPC) {
-		getMnemonic(walletRPC, keyIncludes, function(walletRPC, mnemonicOutput) {
-			if (mnemonicOutput.keyIncluded) { return mnemonic; }
-			else {
-				fs.unlink("./wallets/"+TheWalletName+".keys", function() { // delete previous new-wallet.key
-					fs.unlink("./wallets/"+TheWalletName, function() { // delete previous new-wallet
-						createMnemonic(walletRPC, keyIncludes);
-					});
-				});
-			}
-		});
 	});
-	
-}
-
+})
+.catch(err => {
+	throw new Error(err);
+});
